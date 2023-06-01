ee905c3da0ec7b6077f6663c416ac542edb6206f
Matt <matt@Matts-Air.local>
2023-06-01 14:25:30 -0400

    chore: adds comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,12 @@ import Landing from "@/components/Landing";
 export default function Home() {
   const { user } = useContext(AuthContext);
 
+  // if not logged in, user should see default landing page at "/"
   if (!user?.userId) {
     return <Landing />;
   }
 
+  // if user is logged in, user will see dashboard with all their guilds/channels
   return (
     <>
       <h1>Hello! {user?.username}</h1>
